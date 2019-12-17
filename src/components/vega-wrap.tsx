6404cf300bb3b9a2e1e3533597dadc1cec32c845
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2019-12-17 12:40:23 -0800

    fixed the dep issues

--- src/components/vega-wrap.tsx
+++ src/components/vega-wrap.tsx
@@ -56,7 +56,6 @@ export default class VegaWrapper extends React.Component<VegaWrapperProps> {
         };
       }
     }
-    console.log(finalSpec);
     return (
       <Vega
         actions={false}
