ba0ea1e1f00cdced62d5f6e55fc8d3529a90e75b
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2020-01-24 20:10:36 -0600

    clear lint

--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -11,7 +11,7 @@ export default class ErrorBoundary extends React.Component<Props, State> {
     this.state = {hasError: false};
   }
 
-  static getDerivedStateFromError(error: any): any {
+  static getDerivedStateFromError(): any {
     // Update state so the next render will show the fallback UI.
     return {hasError: true};
   }
