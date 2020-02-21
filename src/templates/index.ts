ca22f17fac2307613bb5f8a029c5e4f4c926000e
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2020-02-21 12:00:47 -0600

    more fixes

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -19,6 +19,7 @@ import ATOM from './atom';
 import BEESWARM_TEMPLATE from './bee-swarm';
 import DATATABLE from './table';
 import PIECHART_TEMPLATE from './pie-chart';
+import GALLERY from './gallery';
 import SCATTERPLOT_TEMPLATE from './scatterplot';
 import {getUserName} from '../utils/local-storage';
 // import SIMPLE_SCATTER from './simple-scatterplot';
@@ -140,6 +141,7 @@ export const DEFAULT_TEMPLATES: Template[] = [
   SCATTERPLOT_TEMPLATE,
   PIECHART_TEMPLATE,
   BEESWARM_TEMPLATE,
+  GALLERY,
   UNITVIS,
   // SIMPLE_SCATTER,
 ];
