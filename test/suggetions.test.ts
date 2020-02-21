cab87dccc07c7f4f9f9ce285fdc181b1dd9dcba3
Andrew McNutt <mcnutt.andrew@gmail.com>
2020-02-21 17:09:45 -0600

    in place suggestion (#66)

--- test/suggetions.test.ts
+++ test/suggetions.test.ts
@@ -1,27 +1,28 @@
 import {IsotypeBarChart, TITANTIC_ETC} from './text-examples';
+import {DataType} from '../src/types';
 import {takeSuggestion} from '../src/utils/index';
 import VEGA_LITE from '../src/languages/vega-lite';
 import UNIT_VIS from '../src/languages/unit-vis';
 
 test('#synthesizeSuggestions happy path - IsotypeBarChart', () => {
   const synthesizeSuggestions = VEGA_LITE.suggestion;
-  const suggestions = synthesizeSuggestions(IsotypeBarChart, []);
+  const suggestions = synthesizeSuggestions(IsotypeBarChart, [], []);
   expect(suggestions).toMatchSnapshot();
   // remove the foreign data
   const newCode = takeSuggestion(IsotypeBarChart, suggestions[3]);
-  const suggestions2 = synthesizeSuggestions(newCode, []);
+  const suggestions2 = synthesizeSuggestions(newCode, [], []);
   expect(suggestions2).toMatchSnapshot();
 
   const newCod2 = takeSuggestion(newCode, suggestions2[2]);
   const widget = suggestions2[2].sideEffect();
   const widgets = [widget];
-  const suggestions3 = synthesizeSuggestions(newCod2, widgets);
+  const suggestions3 = synthesizeSuggestions(newCod2, widgets, []);
   expect(suggestions3).toMatchSnapshot();
 });
 
 test('#synthesizeSuggestions (remove data first) - IsotypeBarChart', () => {
   const synthesizeSuggestions = VEGA_LITE.suggestion;
-  const suggestions = synthesizeSuggestions(IsotypeBarChart, []);
+  const suggestions = synthesizeSuggestions(IsotypeBarChart, [], []);
   expect(suggestions).toMatchSnapshot();
   // remove the foreign data
   const newCode = takeSuggestion(IsotypeBarChart, suggestions[0]);
@@ -29,13 +30,21 @@ test('#synthesizeSuggestions (remove data first) - IsotypeBarChart', () => {
   const widget = suggestions[0].sideEffect();
   const widgets = [widget];
   // take a suggestion
-  const suggestions2 = synthesizeSuggestions(newCode, widgets);
+  const suggestions2 = synthesizeSuggestions(newCode, widgets, [
+    {
+      field: 'animal',
+      type: 'DIMENSION' as DataType,
+      originalType: 'DIMENSION' as DataType,
+      domain: [],
+      summary: {},
+    },
+  ]);
   expect(suggestions2).toMatchSnapshot();
 });
 
 test('#synthesizeSuggestions - Titantic Etc', () => {
   const synthesizeSuggestions = UNIT_VIS.suggestion;
-  const suggestions = synthesizeSuggestions(TITANTIC_ETC, []);
+  const suggestions = synthesizeSuggestions(TITANTIC_ETC, [], []);
   expect(suggestions).toMatchSnapshot();
   // remove the foreign data
   const newCode = takeSuggestion(IsotypeBarChart, suggestions[0]);
@@ -43,6 +52,6 @@ test('#synthesizeSuggestions - Titantic Etc', () => {
   const widget = suggestions[0].sideEffect();
   const widgets = [widget];
   // take a suggestion
-  const suggestions2 = synthesizeSuggestions(newCode, widgets);
+  const suggestions2 = synthesizeSuggestions(newCode, widgets, []);
   expect(suggestions2).toMatchSnapshot();
 });
