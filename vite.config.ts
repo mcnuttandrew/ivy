b6eb0b925a2d80168f634f0db0b62e55f04bfbae
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2023-08-29 16:35:52 -0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,34 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// for production builds swap to using the published version of prong editor, rather than the local one
+function fixStringify() {
+  return {
+    name: 'fix-stringify',
+
+    transform(src, id) {
+      if (id.includes('utils/stringify.ts')) {
+        const code = src.replace(
+          'const stringify = require("json-stringify-pretty-compact");',
+          "import stringify from 'json-stringify-pretty-compact';",
+        );
+        return {code, map: null};
+      }
+    },
+  };
+}
+
 export default defineConfig({
   optimizeDeps: {
     include: ['react/jsx-runtime'],
   },
   assetsInclude: ['**/*.md'],
-  plugins: [react()],
+  plugins: [react(), fixStringify()],
   build: {
-        minify: false
-    },
+    minify: false,
+  },
   define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
+    // By default, Vite doesn't include shims for NodeJS
     global: {},
   },
 });
